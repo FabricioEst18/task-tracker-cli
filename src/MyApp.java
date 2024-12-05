60170b62da8badfbfff3bceea940b8be50b44a72
Fabricio Cesar <fabricioestevam46@gmail.com>
2024-12-05 16:17:22 -0300

    Build the first version of app

--- src/MyApp.java
+++ src/MyApp.java
@@ -1,2 +1,7 @@
 public class MyApp {
+    public static void main(String[] args) {
+        TaskTracker taskTracker = new TaskTracker();
+        taskTracker.listTasks("Done");
+        taskTracker.quit();
+    }
 }
--- src/Task.java
+++ src/Task.java
@@ -1,23 +1,57 @@
+import java.util.UUID;
+
 public class Task {
     private String id;
     private String description;
     private Status status;
 
-    public Task(String id, String description, Status status) {
-        this.id = id;
+    public Task(String description){
+        this.id = UUID.randomUUID().toString();
         this.description = description;
-        this.status = status;
+        this.status = Status.NOT_DONE;
     }
 
-    public void markInProgress() {
+    public String getId(){
+        return id;
+    }
+    public void markInProgress(){
         this.status = Status.IN_PROGRESS;
     }
 
-    public void markDone() {
+    public void markDone(){
         this.status = Status.DONE;
     }
 
-    public void updateDescription(String description) {
+    public void updateDescription(String description){
         this.description = description;
+    }
+    public String toJson(){
+        return "{\"id\":\"" + id.strip() + "\", \"description\":\"" + description.strip() + "\", \"status\":\"" + status.toString() + "\"}";
+    }
+    public static Task fromJson(String json){
+        String[] json1 = json.replace("{", "").replace("}", "")
+                .replace("\"", "").replace(":", ",").split(",");
+        String id = json1[1];
+        String description = json1[3];
+        Status status;
+        String s = json1[5].strip();
+        if (s.equals("Not done")){
+            status = Status.NOT_DONE;
+        } else if (s.equals("In progress")){
+            status = Status.IN_PROGRESS;
+        } else {
+            status = Status.DONE;
+        }
+        Task task = new Task(description);
+        task.id = id;
+        task.status = status;
+        return task;
+    }
+    public Status getStatus(){
+        return status;
+    }
+    @Override
+    public String toString() {
+        return "id: " + id.strip() + ", description: " + description.strip() + ", status: " + status.toString();
     }
 }
